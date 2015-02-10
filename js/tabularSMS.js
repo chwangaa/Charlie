4feb13934854293633b1fcaf69590831b7edebb5
Tanvi Potdar <tanvipotdar@users.noreply.github.com>
2015-02-10 22:59:47 +0000

    Returns an array of objects rather than strings

--- js/tabularSMS.js
+++ js/tabularSMS.js
@@ -1,11 +1,10 @@
-		function findSMS(keyWord, dataArray){
+	function findSMS(keyWord, dataArray){
             var smsArray=[];
             var reg =  (" " + keyWord + " ")
             for(var i=0; i< dataArray.length; i++){
                 for(key in dataArray[i]){
                      if(key=="SMS" && dataArray[i][key].toLowerCase().search(keyWord)!=-1) {
-                        //console.log(JSON.stringify(dataArray[i]));
-                        smsArray.push(JSON.stringify(dataArray[i])+"<br>");
+                        smsArray.push(dataArray[i]);
                     }
                 }
         }
