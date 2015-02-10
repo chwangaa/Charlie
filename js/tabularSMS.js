98594e4f4da1af61a412b5afa3dbbb9dde414108
tanvipotdar <tanu.613@gmail.com>
2015-02-10 14:05:12 +0000

    added a javascript file to handle event listeners for words in the word cloud

---
+++ js/tabularSMS.js
@@ -0,0 +1,22 @@
+
+
+		function findSMS(keyWord, dataArray){
+            var smsArray=[];
+            var reg =  (" " + keyWord + " ")
+            for(var i=0; i< dataArray.length; i++){
+                for(key in dataArray[i]){
+                     if(key=="SMS" && dataArray[i][key].toLowerCase().search(keyWord)!=-1) {
+                        //console.log(JSON.stringify(dataArray[i]));
+                        smsArray.push(JSON.stringify(dataArray[i])+"<br>");
+                    }
+                }
+        }
+        return smsArray;
+      }
+
+      document.getElementById("cloud_tags").addEventListener("click", function(e)
+      {
+          document.getElementById("displayWord").innerHTML = "You clicked on "+e.target.id;
+          document.getElementById("demo").innerHTML =
+          findSMS(e.target.id, data_raw)
+      });
