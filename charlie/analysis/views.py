33e1263c3dd3ee8a1b92685703e47adaa7a6d107
Hang <wangchihang@gmail.com>
2015-02-18 17:32:18 +0000

    Initialization of manipulation table

--- charlie/analysis/views.py
+++ charlie/analysis/views.py
@@ -85,9 +85,7 @@ def analysis(request, datasource_id):
     opinions_raw = sms_set.all().values_list('opinion',flat=True).distinct()
     # cast from ustr to str
     opinions = [str(o) for o in opinions_raw]
-    print opinions
     opinions.append('irrelevant')
-    print opinions
     table = render_to_string("table.html", {"data": data, "opinions": opinions})
     # get the word frequency list
     from utils import getFrequencyList
