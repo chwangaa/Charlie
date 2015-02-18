2988d59257e97d92f3d2ebb2f9a4a0c3b480eab0
Daria Dicu <daria.dicu@gmail.com>
2015-02-18 20:28:44 +0000

    Rescaled weights to a fixed interval

--- charlie/analysis/static/analysis/js/wordcloud.js
+++ charlie/analysis/static/analysis/js/wordcloud.js
@@ -20,6 +20,21 @@ function setUpCanvas() {
 }
 
 /*
+    Scale the frequencies to be in the range 20-120 (font size), but keep proportions.
+    Values are chosen after trying different weight ranges, so they are hardcoded.
+*/
+function reWeigh(words) {
+    var highest_freq = words[0].weight;
+    var lowest_freq = words[words.length - 1].weight;
+    var interval = highest_freq - lowest_freq;
+    for (var i = 0; i < words.length; i++) {
+        // Rescale by (100/interval) and translate by 20 (minimum weight).
+        words[i].weight = 20 + Math.round(((words[i].weight - lowest_freq) * 100) / interval);
+    }
+    return words;
+}
+
+/*
     Appends list of words into HTML as a list, then calls function to set up canvas.
     The tag container should be different than the canvas container, as required by the TagCanvas library.
     TODO(Daria): Pass appropriate container here (might need multiple ones).
@@ -27,12 +42,19 @@ function setUpCanvas() {
 function drawCloudMap(words, container) {
     var colour;
     words = words.slice(0,30);
+
+    // TODO: Remove this, but only once strings only containing whitespace are filtered elsewhere!
+    words = words.filter(function(element) {
+        return element.text.replace(/\s+/g, ' ') != "";
+    });
+
+    words = reWeigh(words);
     list = "<ul>";
-    for(w in words){
-        text = words[w].text;
-        weight = words[w].weight;
-        topic = words[w].topic;
-        if (words[w].topic != null) {
+    for(var i = 0; i < words.length; i++) {
+        text = words[i].text;
+        weight = words[i].weight;
+        topic = words[i].topic;
+        if (words[i].topic != null) {
             colour = topicColour[topic];
         } else {
             colour = topicColour[0];
