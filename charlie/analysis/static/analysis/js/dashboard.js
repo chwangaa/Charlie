7200bdb4099fcfb6068d6c1c3dbc215480104a0b
andrewwang7 <andrew-w@outlook.com>
2015-02-18 21:24:59 +0000

    small interface changes
    upload form, navbar

--- charlie/analysis/static/analysis/js/dashboard.js
+++ charlie/analysis/static/analysis/js/dashboard.js
@@ -3,3 +3,14 @@ document.getElementById('addbutton').addEventListener("click",
                 document.getElementById("overlay").style.display='block';
 				document.getElementById("upload_data").style.display='block';  
         });
+		
+document.getElementById('cancel').addEventListener("click",
+            function(e){
+                document.getElementById("overlay").style.display='none';
+				document.getElementById("upload_data").style.display='none';  
+        });
+		
+document.getElementById('delete').addEventListener("click",
+	function(e){
+		//TODO: Remove data from data source, rerender page
+	});
