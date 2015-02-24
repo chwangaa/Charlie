41f68c9cf1c0a67f01e1bb2101370fe84e63bed5
andrewwang7 <andrew-w@outlook.com>
2015-02-24 11:26:41 +0000

    dropdown menu bug fix
    was hidden behind chart

--- charlie/analysis/static/analysis/js/menutoggle.js
+++ charlie/analysis/static/analysis/js/menutoggle.js
@@ -4,23 +4,19 @@ document.getElementById('toggle').addEventListener("click",
 			if (fullscreen==false){
 				$("#wrapper").toggleClass("toggled");
 				document.getElementById('toggle').className="glyphicon glyphicon-resize-small full_icon";
+				document.getElementById('navbar').style.zIndex=-1;
 				document.getElementById('page-content-wrapper').style.top = 0;
 				document.getElementById('page-content-wrapper').style.left = 0;
-				document.getElementById('page-content-wrapper').style.right = 0;				
+				document.getElementById('page-content-wrapper').style.right = 0;
 				fullscreen = true;
 			}else{				
 				$("#wrapper").toggleClass("toggled");
-				document.getElementById('toggle').className="glyphicon glyphicon-fullscreen full_icon";	
+				document.getElementById('toggle').className="glyphicon glyphicon-fullscreen full_icon";
+				document.getElementById('navbar').style.zIndex=5;
 				document.getElementById('page-content-wrapper').style.top = "50px";
 				document.getElementById('page-content-wrapper').style.left = "250px";
 				fullscreen = false;
 			}
 			
         });
-		
-document.getElementById('refresh').addEventListener("click",
-			function(e){
-				location.reload();
-			});
-
 $("#toggle").trigger("click");
